e2cf995fb33229b34ceacbf3ecc6ca1a820afc94
Alexander <stallev@yahoo.com>
2021-07-06 09:56:43 +0300

    1

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,121 @@
 
+  $('.material-card__variety-slider').each(function() {
+    $(this).slick({
+      slidesToShow: 4,
+      margin: 45,
+      slidesToScroll: 1,               
+      dots: false,
+      arrows: false,
+      infinite: true,
+      responsive: [
+        
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            centerMode: true,
+            centerPadding: '70px',
+          }
+        },
+        {
+          breakpoint: 450,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            centerMode: true,
+            centerPadding: '40px',
+          }
+        },
+      ]
+    });
+});
+
+//увеличение центральной картинки
+
+$('.examples__list').each(function() {
+  $(this).slick({
+    slidesToShow: 3,
+    speed: 500,
+    margin: 45,
+    slidesToScroll: 1, 
+    centerMode: true,
+    focusOnSelect: true,              
+    dots: false,
+    arrows: true,
+    infinite: true,
+    variableWidth: true,
+    cssEase: 'linear', 
+    prevArrow:'<button class="examples-slider-prev"><i class="far fa-angle-left"></i></button>',
+    nextArrow:'<button class="examples-slider-next"><i class="far fa-angle-right"></i></button>',
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: false,
+          focusOnSelect: false,
+          variableWidth: false, 
+        }
+      },
+    ]
+  });
+});
+
+
+
+$('.reviews__list').each(function() {
+  $(this).slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,              
+    dots: false,
+    arrows: true,
+    infinite: true,
+    //centerMode: true,
+    focusOnSelect: true,
+    //variableWidth: true,
+    prevArrow:'<button class="reviews__slider-prev"><i class="far fa-angle-left"></i></button>',
+    nextArrow:'<button class="reviews__slider-next"><i class="far fa-angle-right"></i></button>',
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
+});
+
+$('.review__slider').each(function() {
+  $(this).slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,               
+    dots: true,
+    arrows: true,
+    infinite: true,
+    prevArrow:'<button class="review__slider-prev"><i class="far fa-angle-left"></i></button>',
+    nextArrow:'<button class="review__slider-next"><i class="far fa-angle-right"></i></button>',
+    responsive: [
+      
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      
+    ]
+  });
+});
+
--- src/js/slick-scripts.js
+++ src/js/slick-scripts.js
@@ -23,72 +23,7 @@ if($('.main-slider__list')){
   });
 }
 
-//слайдер на главной в сеции скидок
-if($('.discounts__sliders')){
-  $('.discounts__slider').slick({
-    slidesToShow: 3,
-    margin: 45,
-    slidesToScroll: 1,               
-    dots: false,
-    infinite: true,
-    prevArrow:'<button class="discount-prev"><i class="far fa-angle-left"></i></button>',
-    nextArrow:'<button class="discount-next"><i class="far fa-angle-right"></i></button>',
-    responsive: [
-      {
-        breakpoint: 1199,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        }
-      },
-      {
-        breakpoint: 993,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 2,
-          arrows: false,
-          dots: true,
-          dotsClass: 'main-slider__slick-dots',
-        }
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          arrows: false,
-          dots: true,
-          dotsClass: 'main-slider__slick-dots',
-        }
-      },
-      {
-        breakpoint: 575,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          arrows: false,
-          dots: true,
-          dotsClass: 'main-slider__slick-dots',
-        }
-      },
-      {
-        breakpoint: 400,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 2,
-          arrows: false,
-          dots: true,
-          dotsClass: 'main-slider__slick-dots',
-        }
-      }
-    ]
-  })
-  //выравнивание слайдов по высоте
-  // .on('setPosition', function (event, slick) {
-  //   slick.$slides.css('height', slick.$slideTrack.height() + 'px');
-  // })
-  ;
-}
+
 //слайдер на главной в секции блога
 if($('.blog-section__slider')){
   $('.blog-section__slider').slick({
