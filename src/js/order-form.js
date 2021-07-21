67923d236efd07361f7a80bf3e313f93b687c454
Alexander <stallev@yahoo.com>
2021-07-21 10:39:13 +0300

    before fixing quiz

--- src/js/order-form.js
+++ src/js/order-form.js
@@ -1,71 +1,74 @@
-//форма заказа
-let orderBtns = document.querySelectorAll('.order-btn');
-let orderFormCloseBtn = document.querySelector('.order-form__icon-close-wrap');
+//конвертируем коллекцию в массив
+function convertToArray(collection){
+  let newArray = Array.prototype.slice.call(collection);
+  return newArray;
+ }
+
+let closeFormBtns = convertToArray(document.querySelectorAll('.closeFormBtn'));
+if(closeFormBtns){
+  closeFormBtns.forEach(
+    btn => {
+      btn.addEventListener('click', function(e){
+        e.preventDefault();
+        document.body.removeAttribute("class");
+      });
+    }
+  )
+}
+
+//клик по месту вне формы
 let orderFormOverlay = document.querySelector('.order-form__overlay');
-let orderFormSubmitBtns = document.querySelectorAll('.order-form__submit');
-let orderFormProductName = document.querySelector('.order-form__name');
-function closeOrderForm(){
-  document.body.classList.remove('order-form-view');
-}
 
-
-if(orderBtns){
-  orderBtns.forEach(
-    btn => {
-      btn.addEventListener('click', function(e){
-        e.preventDefault();
-        document.body.classList.add('order-form-view');
-        if(orderFormProductName){
-          orderFormProductName.value = btn.querySelector('.product-card__title-hidden').innerHTML;
-          console.log(orderFormProductName);
-          console.log(orderFormProductName.value);
-        }
-      })
-    }
-  )
-}
-if(orderFormSubmitBtns){
-  orderFormSubmitBtns.forEach(
-    btn => {
-      btn.addEventListener('click', function(e){
-        closeOrderForm();
-        //setTimeout(closeOrderForm, 100);
-      })
-    }
-  )
-}
-if(orderFormCloseBtn){
-  orderFormCloseBtn.addEventListener('click', function(){
-    closeOrderForm();
-  });
-}
 if(orderFormOverlay){
   orderFormOverlay.addEventListener('click', function(){
-    closeOrderForm();
+    document.body.removeAttribute("class");
   });
 }
 
+//вызов форм для калькуляторов заборов и ворот, квиза
+function openCalcForm(btnClass){
+  let btnsCalc = convertToArray(document.querySelectorAll('.'+btnClass));
+  if(btnsCalc){
+    btnsCalc.forEach(
+      btn => {
+        btn.addEventListener('click', function(e){
+          e.preventDefault();
+          document.body.classList.add('order-form-view');
+          document.body.classList.add(btnClass);
+        });
+      }
+    )
+  }
+}
+openCalcForm('palisade-1');
+openCalcForm('gateway-1');
+openCalcForm('quiz');
+//закрытие формы квиза
+let submitQuizBtn = document.querySelector('.quiz__action--submit');
+if(submitQuizBtn){
+  submitQuizBtn.addEventListener('click', function(){
+    document.body.removeAttribute("class");
+  });
+}
 
-//форма заказа звонка
-let callBtns = document.querySelectorAll('.call-btn');
-let callFormCloseBtn = document.querySelector('.call-form__icon-close-wrap');
-if(callBtns){
-  callBtns.forEach(
-    btn => {
-      btn.addEventListener('click', function(e){
-        e.preventDefault();
-        document.body.classList.add('order-form-view-call');
-      })
+//скрытие списка полей
+let fieldGroupTitles = convertToArray(document.querySelectorAll('.order-form__field-group-title'));
+if(fieldGroupTitles){
+  fieldGroupTitles.forEach(
+    fieldGroupTitle => {
+      fieldGroupTitle.addEventListener('click', function(){
+        fieldGroupTitle.classList.toggle('order-form__field-group-title--hidden');
+      });
     }
   )
 }
-if(callFormCloseBtn){
-  callFormCloseBtn.addEventListener('click', function(){
-    document.body.classList.remove('order-form-view-call');
-  });
-}
-if(orderFormOverlay){
-  orderFormOverlay.addEventListener('click', function(){
-    document.body.classList.remove('order-form-view-call');
-  });
-}
+
+//форма заказа расчета забора1
+// let palisade1Btn = document.querySelector('.palisade-1');
+// palisade1Btn.addEventListener('click', function(e){
+//   e.preventDefault();
+//   document.body.classList.add('order-form-view');
+//   document.body.classList.add('palisade-1');
+// });
+//форма заказа расчета забора2
+
--- src/js/script.js
+++ src/js/script.js
@@ -1,42 +1,133 @@
-
-  $('.material-card__variety-slider').each(function() {
-    $(this).slick({
-      slidesToShow: 4,
-      margin: 45,
-      slidesToScroll: 1,               
-      dots: false,
-      arrows: false,
-      infinite: true,
-      responsive: [
+//квиз
+let quizNextBtn1 = document.querySelector('.quiz__action--1');
+let btnsQuizNextStep = convertToArray(document.querySelectorAll('.quiz__action-next'));
+if(btnsQuizNextStep){
+  btnsQuizNextStep.forEach(
+    btn => {
+      btn.addEventListener('click', function(e){
+        e.preventDefault();
+        let mainParent = e.target.closest('.quiz__item');
+        console.log(mainParent.nextElementSibling);
+        //валидация полей на пустое значение
+        let parentsInputs = convertToArray(mainParent.querySelectorAll('input'));
         
-        {
-          breakpoint: 991,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
+        let parentsInputsValidity = true;
+        
+        let validity;
+        if(parentsInputs){
+          for(let i = 0; i<parentsInputs.length; i++){
+            validity = parentsInputs[i].validity.valid;
+            if(!validity) return;
           }
-        },
-        {
-          breakpoint: 767,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            centerMode: true,
-            centerPadding: '70px',
-          }
-        },
-        {
-          breakpoint: 450,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            centerMode: true,
-            centerPadding: '40px',
-          }
-        },
-      ]
-    });
-});
+          parentsInputsValidity = validity;
+        }
+        let parentsSelect = convertToArray(mainParent.querySelectorAll('select'));
+        let parentsSelectsValidity = true;
+        // if(parentsSelect){
+        //   for(let i = 0; i<parentsSelect.length; i++){
+        //     if(parentsSelect[i].value == ""){
+        //      validity = false; 
+        //     }
+        //     else validity = true;
+        //     if(!validity) return;
+        //   }
+        //   parentsSelectsValidity = validity;
+        // }
+        if(parentsInputsValidity&&parentsSelectsValidity){
+          mainParent.nextElementSibling.style.display = "flex";
+          mainParent.style.display = "none";
+        }
+        mainParent.nextElementSibling.style.display = "flex";
+        mainParent.style.display = "none";
+      });
+    }
+  )
+}
+//слайдеры материалов забора
+document.addEventListener( 'DOMContentLoaded', function () {
+  new Splide( '.material-card__variety-slider--1', {
+    type: 'loop',
+    focus    : 'center',
+    pagination: false,
+    arrows: false,
+    //gap: '5px',
+    perPage    : 4,
+    breakpoints: {
+      991: {
+        perPage: 3,
+      },
+      768: {
+        perPage: 2,
+      },
+      450: {
+        perPage: 1,
+      },
+    }
+  } ).mount();
+} );
+document.addEventListener( 'DOMContentLoaded', function () {
+  new Splide( '.material-card__variety-slider--2', {
+    type: 'loop',
+    focus    : 'center',
+    pagination: false,
+    arrows: false,
+    gap: '35px',
+    perPage    : 4,
+    breakpoints: {
+      991: {
+        perPage: 3,
+      },
+      768: {
+        perPage: 2,
+      },
+      450: {
+        perPage: 1,
+      },
+    }
+  } ).mount();
+} );
+document.addEventListener( 'DOMContentLoaded', function () {
+  new Splide( '.material-card__variety-slider--3', {
+    type: 'loop',
+    focus    : 'center',
+    pagination: false,
+    arrows: false,
+    gap: '25px',
+    perPage    : 4,
+    breakpoints: {
+      991: {
+        perPage: 3,
+      },
+      768: {
+        perPage: 2,
+      },
+      450: {
+        perPage: 1,
+      },
+    }
+  } ).mount();
+} );
+document.addEventListener( 'DOMContentLoaded', function () {
+  new Splide( '.material-card__variety-slider--4', {
+    type: 'loop',
+    focus    : 'center',
+    pagination: false,
+    arrows: false,
+    gap: '25px',
+    perPage    : 4,
+    breakpoints: {
+      991: {
+        perPage: 3,
+      },
+      768: {
+        perPage: 2,
+      },
+      450: {
+        perPage: 1,
+      },
+    }
+  } ).mount();
+} );
 
 //увеличение центральной картинки
 
@@ -53,8 +144,8 @@ $('.examples__list').each(function() {
     infinite: true,
     variableWidth: true,
     cssEase: 'linear', 
-    prevArrow:'<button class="examples-slider-prev"><i class="far fa-angle-left"></i></button>',
-    nextArrow:'<button class="examples-slider-next"><i class="far fa-angle-right"></i></button>',
+    prevArrow:'<button class="examples-slider-prev"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40" width="40" height="40"><path d="m15.5 0.932-4.3 4.38 14.5 14.6-14.5 14.5 4.3 4.4 14.6-14.6 4.4-4.3-4.4-4.4-14.6-14.6z"></path></svg></button>',
+    nextArrow:'<button class="examples-slider-next"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40" width="40" height="40"><path d="m15.5 0.932-4.3 4.38 14.5 14.6-14.5 14.5 4.3 4.4 14.6-14.6 4.4-4.3-4.4-4.4-14.6-14.6z"></path></svg></button>',
     responsive: [
       {
         breakpoint: 767,
@@ -70,48 +161,55 @@ $('.examples__list').each(function() {
   });
 });
 
-$('.reviews__list').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,              
-  dots: false,
-  arrows: true,
-  infinite: true,
-  centerMode: true,
-  //variableWidth: true,
-  focusOnSelect: true,
-  prevArrow:'<button class="reviews__slider-prev"><i class="far fa-angle-left"></i></button>',
-  nextArrow:'<button class="reviews__slider-next"><i class="far fa-angle-right"></i></button>',
-  responsive: [
-    {
-      breakpoint: 767,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      }
-    },
-  ]
-});
 
-$('.review__slider').each(function() {
-  $(this).slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,               
-    dots: true,
-    arrows: true,
-    infinite: true,
-    prevArrow:'<button class="review__slider-prev"><i class="far fa-angle-left"></i></button>',
-    nextArrow:'<button class="review__slider-next"><i class="far fa-angle-right"></i></button>',
-    responsive: [
-      
-      {
-        breakpoint: 991,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        }
+document.addEventListener( 'DOMContentLoaded', function () {
+  new Splide( '.reviews__slider', {
+    type: 'loop',
+    focus    : 'center',
+    pagination: false,
+    width: '100%',
+    start: 1,
+    perPage    : 3,
+    breakpoints: {
+      768: {
+        perPage: 1,
+        width: '60%',
       },
-      
-    ]
-  });
-});
+      575: {
+        perPage: 1,
+        width: '90%',
+      },
+      350: {
+        perPage: 1,
+        width: '90%',
+      },
+    }
+  } ).mount();
+} );
 
+function convertToArray(collection){
+  let newArray = Array.prototype.slice.call(collection);
+  return newArray;
+ }
+
+document.addEventListener( 'DOMContentLoaded', function () {
+  let subSliderItems = document.querySelectorAll('.review__slider');
+  //let subSliderItems = convertToArray(subSliderItems);
+  for(let i=0; i < subSliderItems.length; i++){
+    subSliderItems[i].classList.add(`sub-slider${i}`);
+    new Splide( `.sub-slider${i}`, {
+      type: 'loop',
+      focus    : 'center',
+      width: '100%',
+      start: 1,
+      perPage    : 1,
+      breakpoints: {
+        575: {
+          perPage: 1,
+          arrows: true,
+        }
+      }
+    } ).mount();
+  }
+  
+} );
